44e279042ea00a501d67534b6cf6c311edb028a0
Johny Wills <jonathan_wills@icloud.com>
2020-11-26 23:06:41 +0000

    added upload button and svg, finished styling, ready to add functionality

--- image-uploader/pages/index.js
+++ image-uploader/pages/index.js
@@ -1,18 +1,40 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+import { useRef } from 'react';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const fileUploader = useRef(null);
 
-      <main className={styles.main}>
-        Image Uploader
-      </main>
-    </div>
-  )
+	const openFileUpload = () => {
+		fileUploader.current.click();
+	};
+
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Image Uploader</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<main className={styles.main}>
+				<h2 className={styles.uploadTitle}>Upload your image</h2>
+				<p className={styles.uploadDesc}>File should be '.jpeg' or '.png'...</p>
+				<div className={styles.imageUploadSection}>
+					<Image src='/image.svg' height='88px' width='114px' />
+					<p className={styles.imageUploadText}>Drag & Drop your image here</p>
+				</div>
+				<p className={styles.or}>Or</p>
+				<input
+					type='file'
+					ref={fileUploader}
+					style={{ display: 'none' }}
+					onChange={(e) => console.log(e.target.files)}
+				/>
+				<button className={styles.fileButton} onClick={openFileUpload}>
+					Choose a file
+				</button>
+			</main>
+		</div>
+	);
 }
